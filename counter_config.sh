8a0509f35f3219c181b2868e9321e55ae9cfc54e
zweer1 <37046835+zweer1@users.noreply.github.com>
2018-03-12 14:10:12 +0200

    Update counter_config.sh

--- counter_config.sh
+++ counter_config.sh
@@ -6,12 +6,12 @@
 # first copy the counter.service file to /etc/systemd/system/
 # and set the relevant permission
 
-chmod 744 counter.service
-cp counter.service /etc/systemd/system/
+chmod 744 /var/lib/jenkins/workspace/Micro_Service_Post_Counter/service/counter.service
+cp /var/lib/jenkins/workspace/Micro_Service_Post_Counter/service/counter.service /etc/systemd/system/
 
 echo counter.service copied to systemd
 # reload the systemd daemon
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 echo systemctl daemon-reload done
 echo " "
